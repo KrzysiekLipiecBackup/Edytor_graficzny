7562e28a5878d5f9980636c61a89ea7e59d6a88f
Krzysiek <krzysiekl57991@gmail.com>
2020-08-23 18:02:13 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+
+namespace Edytor_graficzny
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			// Create the startup window
+			MainWindow wnd = new MainWindow();
+			// Do stuff here, e.g. to the window
+			if (e.Args.Length == 1)
+				MessageBox.Show("Found Secret #1\n\nAdded Parameter: \n\n" + e.Args[0]);
+			wnd.Title = "Hello World";
+			// Show the window
+			wnd.Show();
+		}
+
+		private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+		{
+			MessageBox.Show("An unhandled exception just occurred: " + e.Exception.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+			e.Handled = true;
+		}
+	}
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+//…more using statements
+
+
+namespace Edytor_graficzny
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickMe_Click(object sender, RoutedEventArgs e)
+		{
+			lbResult.Items.Add(pnlMain.FindResource("strPanel").ToString());
+			lbResult.Items.Add(this.FindResource("strWindow").ToString());
+			lbResult.Items.Add(Application.Current.FindResource("strApp").ToString());
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			string s = null;
+			try
+			{
+				s.Trim();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("A handled exception just occurred: " + ex.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+			}
+			s.Trim();
+		}
+	}
+}
